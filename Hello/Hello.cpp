cb98cc43fd48b66adf2f4d242a441f17e51dd50d
Tanya <tanyakovalenko1994@gmail.com>
2015-10-17 09:36:02 -0400

    Work with arguments

--- Hello/Hello.cpp
+++ Hello/Hello.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]){ 
-    int a, b, c;
-    cin >> a >> b >> c;   
-    cout << a << endl;
+int main(int argc, char* argv[]) {
+	// если передаем аргументы, то argc будет больше 1(в зависимости от кол-ва аргументов) 
+	if (argc > 1) {
+		cout << argv[1]<<endl;// вывод второй строки из массива указателей на строки(нумерация в строках начинается с 0 )
+	} else {
+		cout << "Not arguments" << endl;
+        }  
     return 0;
 }
